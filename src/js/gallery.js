89f89c5b86959019001f16d1c03a169c53e03769
Bilychuk Kateryna <bilychukkateryna@ukr.net>
2024-03-09 16:49:36 +0200

    done-gallery

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -3,12 +3,30 @@ import { createGalleryCard } from './createGallery';
 import { refs } from './refs';
 import Pagination from 'tui-pagination';
 import 'tui-pagination/dist/tui-pagination.min.css';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 
 const options = { 
     totalItems: 0,
     itemsPerPage: 12,
     visiblePages: 5,
     page: 1,
+    template: {
+         page: '<a href="#" class="tui-page-btn">{{page}}</a>',
+         currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+         moveButton:
+             '<a href="#" class="tui-page-btn tui-{{type}}">' +
+                 '<span class="tui-ico-{{type}}">{{type}}</span>' +
+             '</a>',
+         disabledMoveButton:
+             '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
+                 '<span class="tui-ico-{{type}}">{{type}}</span>' +
+             '</span>',
+         moreButton:
+             '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
+                 '<span class="tui-ico-ellip">...</span>' +
+             '</a>'
+     }
 }
 
 const pagination = new Pagination(refs.paginationContainer, options);
@@ -23,7 +41,9 @@ service.fetchPopularPhotos(page)
         pagination.reset(total);
     });
 
-pagination.on('afterMove', (event) => {
+pagination.on('afterMove', popular);
+
+function popular(event) {
      const currentPage = event.page;
     service.fetchPopularPhotos(currentPage)
         .then(({ results }) => {
@@ -31,26 +51,63 @@ pagination.on('afterMove', (event) => {
         refs.gallery.innerHTML = markup;
             }
         );
-});
+}
 
 refs.searchForm.addEventListener("submit", searchFormFoto);
 
 function searchFormFoto(event) {
     event.preventDefault();
+    
     const searchQuery = event.target.elements.query.value.trim();
     if (!searchQuery) {
+        iziToast.warning({
+        title: 'Caution',
+        message: 'Please enter query word',
+        });
         return;
     }
+    pagination.off('afterMove', popular);
+    pagination.off('afterMove', byQuery);
     service.query = searchQuery;
     service.fetchFotosByQuery(page)
         .then(({ total, results }) => {
             if (results.length === 0) {
+                iziToast.warning({
+                title: 'Caution',
+                message: 'Please try another word',
+                });
                 return
+            }
+            if (total <= 12) {
+                refs.paginationContainer.style.display = 'none';
+            } else {
+                refs.paginationContainer.style.display = 'block'
             }
         const markup = createGalleryCard(results)
         refs.gallery.innerHTML = markup;
+        iziToast.success({
+        title: 'OK',
+        message: `We found ${total} images`,
+        });    
         pagination.reset(total);
+    }   
+    ).catch( (error) => {
+        iziToast.error({
+        title: 'Error',
+        message: 'Illegal operation',
+        });
     }
-        
     )
+    pagination.on('afterMove', byQuery);
+    searchForm.reset()
 };
+
+function byQuery(event) {
+     const currentPage = event.page;
+    service.fetchFotosByQuery(currentPage)
+        .then(({ results }) => {
+        const markup = createGalleryCard(results)
+        refs.gallery.innerHTML = markup;
+            }
+        );
+}
