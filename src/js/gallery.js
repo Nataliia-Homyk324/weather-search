56d125be19054be593857cc67abb7134c6346b79
Nataliia-Homyk324 <homyk324@gmail.com>
2024-03-11 17:48:06 +0200

    end

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -11,22 +11,22 @@ const options = {
     itemsPerPage: 12,
     visiblePages: 5,
     page: 1,
-    template: {
-         page: '<a href="#" class="tui-page-btn">{{page}}</a>',
-         currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
-         moveButton:
-             '<a href="#" class="tui-page-btn tui-{{type}}">' +
-                 '<span class="tui-ico-{{type}}">{{type}}</span>' +
-             '</a>',
-         disabledMoveButton:
-             '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
-                 '<span class="tui-ico-{{type}}">{{type}}</span>' +
-             '</span>',
-         moreButton:
-             '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
-                 '<span class="tui-ico-ellip">...</span>' +
-             '</a>'
-     }
+    // template: {
+    //      page: '<a href="#" class="tui-page-btn">{{page}}</a>',
+    //      currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+    //      moveButton:
+    //          '<a href="#" class="tui-page-btn tui-{{type}}">' +
+    //              '<span class="tui-ico-{{type}}">{{type}}</span>' +
+    //          '</a>',
+    //      disabledMoveButton:
+    //          '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
+    //              '<span class="tui-ico-{{type}}">{{type}}</span>' +
+    //          '</span>',
+    //      moreButton:
+    //          '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
+    //              '<span class="tui-ico-ellip">...</span>' +
+    //          '</a>'
+    //  }
 }
 
 const pagination = new Pagination(refs.paginationContainer, options);
